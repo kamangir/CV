e1b1b05a9d4f4983e16aab1017cff80bc9b1cb07
AAbadpour <aabadpour@fio.com>
2019-01-22 15:43:15 -0500

    Moved output pdf files to root.

--- src/build.bat
+++ src/build.bat
@@ -1,7 +1,9 @@
 @echo off
 
-rem Revision 1.01
+rem Revision 1.02
 
 call ..\..\Dec8\src\mypy\latex2pdf cv.tex %cd%\ -bibtex
+move cv.pdf ../
 
 call ..\..\Dec8\src\mypy\latex2pdf cv-full.tex %cd%\ -bibtex
+move cv-full.pdf ../
