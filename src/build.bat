09cbf103efb9faae63fb3abde2ea4d62d9cf8bb8
AAbadpour <aabadpour@fio.com>
2019-04-01 12:05:51 -0400

    #2

--- src/build.bat
+++ src/build.bat
@@ -1,9 +1,5 @@
-@echo off
-
-rem Revision 1.02
-
-call ..\..\Dec8\src\mypy\latex2pdf cv.tex %cd%\ -bibtex
+call ..\..\mypy\latex2pdf cv.tex %cd%\ -bibtex
 move cv.pdf ../
 
-call ..\..\Dec8\src\mypy\latex2pdf cv-full.tex %cd%\ -bibtex
+call ..\..\mypy\latex2pdf cv-full.tex %cd%\ -bibtex
 move cv-full.pdf ../
