c7c7ad58c9e82646b4b34d280b85b15923b0233d
aabadpour <aabadpour@betterview.com>
2020-07-27 14:42:31 -0700

    #13

--- src/build.sh
+++ src/build.sh
@@ -1,19 +1,15 @@
-filename=cv
+for filename in cv cv_full; do
+    rm $filename.dvi
+    rm $filename.ps
+    rm $filename.pdf
 
-rm $filename.dvi
-rm $filename.ps
-rm $filename.pdf
+    "latex" -interaction=nonstopmode $filename.tex
 
-"latex" -interaction=nonstopmode $filename.tex
+    "makeindex"$filename.idx
 
-"makeindex"$filename.idx
+    "dvips" -o $filename.ps $filename.dvi
 
-"dvips" -o $filename.ps $filename.dvi
+    "ps2pdf" $filename.ps
 
-"ps2pdf" $filename.ps
-
-# ./ -bibtex
-#cp /y cv.pdf ../
-
-#bash ../../mypy/latex2pdf.sh cv-full.tex ./ -bibtex
-#cp cv-full.pdf ../
+    cp $filename.pdf ../
+end
