b9a1ca855b8feb671cb06ea969c5c0a92993d76d
aabadpour <aabadpour@betterview.com>
2020-07-27 14:39:05 -0700

    #13

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,13 @@
-bash ../../mypy/latex2pdf.sh cv.tex ./ -bibtex
-cp /y cv.pdf ../
+filename=cv
 
-bash ../../mypy/latex2pdf.sh cv-full.tex ./ -bibtex
-cp cv-full.pdf ../
+rm $filename.dvi
+rm $filename.ps
+rm $filename.pdf
+
+"latex" -interaction=nonstopmode $filename.tex
+
+# ./ -bibtex
+#cp /y cv.pdf ../
+
+#bash ../../mypy/latex2pdf.sh cv-full.tex ./ -bibtex
+#cp cv-full.pdf ../
