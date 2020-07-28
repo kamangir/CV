102fc82e26230f4eb9120e0a88e6289c68d1c620
aabadpour <aabadpour@betterview.com>
2020-07-28 12:49:01 -0700

    #14

---
+++ src/build.py
@@ -0,0 +1,27 @@
+import sys
+
+sys.path.append("../../mypy")
+import File
+import String
+
+success, _revision_tex = File.load_text("_revision.tex")
+if success:
+    current_revision = [float(String.after(string, "\\space")) for string in _revision_tex if "Revision" in string]
+    success = len(current_revision) == 1
+    if not success:
+        print(f"{len(current_revision)} current revision(s).")
+        exit
+
+if success:
+    print(f"current revision: {current_revision[0]:.2f}")
+
+    new_revision = current_revision[0] + 0.01
+    print(f"new revision: {new_revision:.2f}")
+
+    _revision_tex = [string.replace(f"{current_revision[0]:.2f}", f"{new_revision:.2f}") for string in _revision_tex]
+    print(f"_revision_tex: {'|'.join(_revision_tex)}")
+
+    success = File.save_text("_revision.tex", _revision_tex)
+
+print("failure,success".split(",")[int(success)])
+
