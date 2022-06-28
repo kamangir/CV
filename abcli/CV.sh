564c8e767d9d79b856ff46f5b7f39601713f1f7e
kamangir <arash@kamangir.net>
2022-06-28 11:52:50 -0400

    CV -> abc - kamangir/bolt#685

--- abcli/CV.sh
+++ abcli/CV.sh
@@ -1,18 +1,21 @@
 #! /usr/bin/env bash
 
-function bolt_CV() {
-    local task=$(bolt_unpack_keyword $1 help)
+function CV() {
+    abcli_CV $@
+}
+
+function abcli_CV() {
+    local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        bolt_help_line "CV build [cv/cv-full] [commit message]" \
+        abcli_help_line "CV build [cv/cv-full] [commit message]" \
             "build CV [and commit w/ message]."
-        bolt_help_line "CV clean" \
+        abcli_help_line "CV clean" \
             "clean CV."
-        bolt_help_line "CV terraform" \
+        abcli_help_line "CV terraform" \
             "terraform CV."
 
-
-        if [ "$(bolt_keyword_is $2 verbose)" == true ] ; then
+        if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m CV --help
         fi
         return
@@ -21,9 +24,9 @@ function bolt_CV() {
     if [ "$task" == "build" ] ; then
         local what="$2"
 
-        bolt_log "building CV... $what"
+        abcli_log "building CV... $what"
 
-        pushd $bolt_path_git/CV/src > /dev/null
+        pushd $abcli_path_git/CV/src > /dev/null
 
         python3 -m CV build
 
@@ -34,7 +37,7 @@ function bolt_CV() {
         local filename
         for filename in cv cv-full; do
             if [ -z "$what" ] || [ "$what" = "$filename" ]; then
-                bolt_log "building $filename..."
+                abcli_log "building $filename..."
                 rm $filename.dvi
                 rm $filename.ps
 
@@ -60,7 +63,7 @@ function bolt_CV() {
         git status
 
         local message="${@:2}" 
-        git commit -a -m "bolt build $message"
+        git commit -a -m "abcli build $message"
 
         git push
 
@@ -75,7 +78,7 @@ function bolt_CV() {
     fi
 
     if [ "$task" == "clean" ] ; then
-        pushd $bolt_path_git/CV/src > /dev/null
+        pushd $abcli_path_git/CV/src > /dev/null
         rm *.aux
         rm *.dvi
         rm *.log
@@ -86,9 +89,9 @@ function bolt_CV() {
     fi
 
     if [ "$task" == "terraform" ] ; then
-        bolt_git terraform CV
+        abcli_git terraform CV
         return
     fi
 
-    bolt_log_error "unknown task: CV '$task'."
+    abcli_log_error "-CV: $task: command not found."
 }
