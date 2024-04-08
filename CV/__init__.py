0b2198a6c5afb5450a4fcc7113396144f4204a21
kamangir <arash@kamangir.net>
2024-04-07 20:04:28 -0700

    test refactor - kamangir/bolt#746

--- CV/__init__.py
+++ CV/__init__.py
@@ -4,4 +4,4 @@ ICON = "📜"
 
 DESCRIPTION = f"{ICON} My Curriculum vitae"
 
-VERSION = "1.60.1"
+VERSION = "1.61.1"
--- CV/__main__.py
+++ CV/__main__.py
@@ -4,12 +4,19 @@ from CV.build import build
 from CV.logger import logger
 
 parser = argparse.ArgumentParser(NAME, description=f"{NAME}-{VERSION}")
-parser.add_argument("task", type=str, help="build")
+parser.add_argument(
+    "task",
+    type=str,
+    help="build|version",
+)
 args = parser.parse_args()
 
 success = False
 if args.task == "build":
     success = build()
+if args.task == "version":
+    print(f"{NAME}-{VERSION}")
+    success = True
 else:
     logger.error(f"-{NAME}: {args.task}: command not found.")
 
