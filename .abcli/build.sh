49ccd38b27bb631ff0c11b3208547463e8b2d08d
kamangir <arash@kamangir.net>
2024-05-11 15:28:24 -0700

    test refactor - kamangir/bolt#746

--- .abcli/build.sh
+++ .abcli/build.sh
@@ -4,7 +4,7 @@ function CV_build() {
     local options=$1
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        local options="dryrun,~publish,what=<cv+cv-full>"
+        local options="dryrun,~publish,~rm,what=<cv+cv-full>"
         abcli_show_usage "CV build [$options]" \
             "build CV."
         return
@@ -12,6 +12,7 @@ function CV_build() {
 
     local do_dryrun=$(abcli_option_int "$options" dryrun 0)
     local do_publish=$(abcli_option_int "$options" publish $(abcli_not $do_dryrun))
+    local do_rm=$(abcli_option_int "$options" rm 1)
     local what=$(abcli_option "$options" what cv+cv-full)
 
     abcli_log "building CV... [$what]"
@@ -32,15 +33,18 @@ function CV_build() {
         abcli_latex build dryrun=$do_dryrun \
             ./$filename.tex
 
-        [[ "$do_publish" == 0 ]] && continue
+        if [[ "$do_publish" == 1 ]]; then
+            public_filename=arash-abadpour-resume
+            [[ "$filename" == *"full"* ]] && public_filename=$public_filename-full
 
-        public_filename=arash-abadpour-resume
-        [[ "$filename" == *"full"* ]] && public_filename=$public_filename-full
+            abcli_eval dryrun=$do_dryrun \
+                aws s3 cp \
+                $filename.pdf \
+                s3://abadpour-com/cv/$public_filename.pdf
+        fi
 
-        abcli_eval dryrun=$do_dryrun \
-            aws s3 cp \
-            $filename.pdf \
-            s3://abadpour-com/cv/$public_filename.pdf
+        [[ "$do_rm" == 1 ]] && rm -v $filename.pdf
+
     done
 
     popd >/dev/null
--- CV/__init__.py
+++ CV/__init__.py
@@ -4,4 +4,4 @@ ICON = "📜"
 
 DESCRIPTION = f"{ICON} My Curriculum vitae"
 
-VERSION = "1.68.1"
+VERSION = "1.69.1"
