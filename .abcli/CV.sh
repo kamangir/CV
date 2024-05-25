76e19b690d687fa8d61b4c0d7469e0f20735f0ad
kamangir <arash@kamangir.net>
2024-05-25 12:41:54 -0700

    refactor - kamangir/bolt#746

--- .abcli/CV.sh
+++ .abcli/CV.sh
@@ -1,46 +1,17 @@
 #! /usr/bin/env bash
 
-function CV() {
-    abcli_CV $@
-}
-
 function abcli_CV() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ]; then
         CV_build "$@"
         CV_clean "$@"
-
-        $(abcli_keyword_is $2 verbose) &&
-            python3 -m abadpour --help
-
-        return 0
-    fi
-
-    local function_name=CV_$task
-    if [[ $(type -t $function_name) == "function" ]]; then
-        $function_name "${@:2}"
         return
     fi
 
-    if [[ "|pylint|pytest|test|" == *"|$task|"* ]]; then
-        abcli_${task} plugin=CV,$2 \
-            "${@:3}"
-        return
-    fi
-
-    if [ "$task" == "init" ]; then
-        abcli_init CV "${@:2}"
-        return
-    fi
-
-    if [ "$task" == "version" ]; then
-        python3 -m abadpour version "${@:2}"
-        return
-    fi
-
-    abcli_log_error "-CV: $task: command not found."
-    return 1
+    abcli_generic_task \
+        plugin=CV,task=$task \
+        "${@:2}"
 }
 
 abcli_source_path \
