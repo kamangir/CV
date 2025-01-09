80a29414e5715a40a5ad6d29b9a0df8718ccdbed
kamangir <arash@kamangir.net>
2025-01-08 20:43:27 -0800

    pipeline fix, hopefully - kamangir/bolt#746

--- abadpour/.abcli/tests/version.sh
+++ abadpour/.abcli/tests/version.sh
@@ -2,10 +2,9 @@
 
 function test_CV_version() {
     local options=$1
-    local do_dryrun=$(abcli_option_int "$options" dryrun 0)
 
-    abcli_eval dryrun=$do_dryrun \
-        "CV version ${@:2}"
+    abcli_eval ,$options \
+        "abcli_CV version ${@:2}"
 
     return 0
 }
