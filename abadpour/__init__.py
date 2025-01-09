6e3c5e7ae4a931c0095c7b27e7e4805519d3fa27
kamangir <arash@kamangir.net>
2025-01-08 22:47:56 -0800

    start of refactors-2025-01-08-5rOTdq 🪄 - kamangir/bolt#746

--- abadpour/__init__.py
+++ abadpour/__init__.py
@@ -4,6 +4,10 @@ ICON = "📜"
 
 DESCRIPTION = f"{ICON} Arash Abadpour's CV."
 
-VERSION = "6.75.1"
+VERSION = "6.76.1"
 
 ALIAS = "CV"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/blob/main/abadpour/2020-11-21.jpg?raw=true"
+)
